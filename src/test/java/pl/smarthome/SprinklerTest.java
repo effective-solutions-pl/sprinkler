b1ef55ee53a6ee3dece686410d94e97ac061518d
effective-solutions-pl <effective-solutions-pl>
2022-04-06 22:13:11 +0000

    Initial commit

---
+++ src/test/java/pl/smarthome/SprinklerTest.java
@@ -0,0 +1,21 @@
+package pl.smarthome;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class SprinklerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
